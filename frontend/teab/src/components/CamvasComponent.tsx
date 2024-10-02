2ccd5948edbaefbc603377cfe0e873ddc1641610
repllca <maridann1022@icloud.com>
2024-10-02 11:05:45 +0900

    first commit

---
+++ frontend/teab/src/components/CamvasComponent.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei'; // カメラの操作をサポートするコンポーネント
+// import Classroom from './Classroom'; // 教室のコンポーネントをインポート
+
+// それぞれの環境設定を行う 
+const CanvasComponent: React.FC = () => {
+  return (
+    <Canvas>
+      {/* 環境光を追加 */}
+      <ambientLight intensity={0.5} />
+      {/* 指向性光を追加 */}
+      <directionalLight position={[0, 10, 5]} intensity={1.5} />
+      {/* 教室コンポーネントを描画 */}
+      {/* <Classroom /> */}
+      {/* カメラ操作のためのOrbitControls */}
+      <OrbitControls />
+    </Canvas>
+  );
+};
+
+export default CanvasComponent;
---
+++ frontend/teab/src/components/Classroom.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { useRef } from 'react';
+import { Mesh } from 'three';
+
+const Classroom: React.FC = () => {
+  // 机を描画する関数
+  const renderDesk = (position: [number, number, number]) => {
+    return (
+      <mesh position={position}>
+        <boxGeometry args={[1.5, 0.1, 1}] />
+        <meshStandardMaterial color="brown" />
+      </mesh>
+    );
+  };
+
+  // 椅子を描画する関数
+  const renderChair = (position: [number, number, number]) => {
+    return (
+      <mesh position={position}>
+        <boxGeometry args={[0.5, 0.1, 0.5]} />
+        <meshStandardMaterial color="darkblue" />
+      </mesh>
+    );
+  };
+
+  return (
+    <>
+      {/* 教室の床 */}
+      <mesh position={[0, -0.1, 0]}>
+        <boxGeometry args={[10, 0.1, 10]} />
+        <meshStandardMaterial color="lightgray" />
+      </mesh>
+
+      {/* 机の配置 */}
+      {renderDesk([-3, 0, -2])}
+      {renderDesk([-3, 0, 0])}
+      {renderDesk([-3, 0, 2])}
+      {renderDesk([3, 0, -2])}
+      {renderDesk([3, 0, 0])}
+      {renderDesk([3, 0, 2])}
+
+      {/* 椅子の配置 */}
+      {renderChair([-3, 0, -2.5])}
+      {renderChair([-3, 0, 0.5])}
+      {renderChair([-3, 0, 2.5])}
+      {renderChair([3, 0, -2.5])}
+      {renderChair([3, 0, 0.5])}
+      {renderChair([3, 0, 2.5])}
+
+      {/* 教壇 */}
+      <mesh position={[0, 0, 4]}>
+        <boxGeometry args={[2, 0.5, 1]} />
+        <meshStandardMaterial color="saddlebrown" />
+      </mesh>
+
+      {/* プロジェクター */}
+      <mesh position={[0, 1, 4]}>
+        <boxGeometry args={[0.5, 0.3, 0.5]} />
+        <meshStandardMaterial color="black" />
+      </mesh>
+    </>
+  );
+};
+
+export default Classroom;
+
